69f63f7b7faa1953a58dbcdbe12473e6fb7fb205
LucasSangino <lucas.sangino@alu.frt.utn.edu.ar>
2026-05-20 14:19:10 -0300

    Se realizaron las tareas de cada caso

--- Dsw2026Ej11/Collections/CasoList.cs
+++ Dsw2026Ej11/Collections/CasoList.cs
@@ -1,4 +1,6 @@
-﻿namespace Dsw2026Ej11.Collections;
+﻿using Dsw2026Ej11.Domain;
+
+namespace Dsw2026Ej11.Collections;
 
 //Crear un campo que represente una lista de alumnos (List<>)
 //Incluir un método para agregar alumnos a la lista
@@ -8,4 +10,34 @@
 //Incluir un método para eliminar un alumno en una determinada posición de la lista
 public class CasoList
 {
+    private List<Alumno> _alumnos;
+
+    public CasoList()
+    {
+        _alumnos = new List<Alumno>();
+    }
+
+    public void AgregarAlumno(List<Alumno> alumnos){
+        _alumnos.AddRange(alumnos);
+    }
+
+    public List<Alumno> GetAlumnos(){
+        return _alumnos;
+    }
+
+    public Alumno? BuscarAlumnoPorNombre(string name) { 
+        return _alumnos.Find(a => a.Nombre == name);
+    }
+
+    public void EliminarDeterminadoAlumno(Alumno alumno) { 
+    
+        _alumnos.Remove(alumno);
+
+    }
+
+    public void EliminarAlumnoEnPoicion(int Indice) { 
+        
+        _alumnos.RemoveAt(Indice);
+
+    }
 }
--- Dsw2026Ej11/Tests/Ejemplos.cs
+++ Dsw2026Ej11/Tests/Ejemplos.cs
@@ -1,4 +1,8 @@
-﻿namespace Dsw2026Ej11.Tests;
+﻿using Dsw2026Ej11.Collections;
+using Dsw2026Ej11.Domain;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dsw2026Ej11.Tests;
 
 
 internal class Ejemplos
@@ -11,7 +15,23 @@ internal class Ejemplos
     //Eliminar el primer elemento de la lista y listar por consola los alumnos
     public static void EjemploList()
     {
+        var lista = new CasoList();
+        List<Alumno> alumnos = new List<Alumno>();
+        alumnos.Add( new Alumno(133, "Pedro",7));
+        alumnos.Add(new Alumno(155, "Nacho", 6));
+        alumnos.Add(new Alumno(444, "Juan", 9));
 
+        lista.AgregarAlumno(alumnos);
+
+        Console.WriteLine($"{lista.BuscarAlumnoPorNombre("Juan")}");
+      
+         Console.WriteLine($"{lista.BuscarAlumnoPorNombre("Ignacio")?.Nombre ?? "No existe"}");
+        lista.EliminarDeterminadoAlumno(alumnos[1]);
+        Console.WriteLine("---------");
+        lista.GetAlumnos().ForEach(Console.WriteLine);
+        Console.WriteLine("---------");
+        lista.EliminarAlumnoEnPoicion(0);
+        lista.GetAlumnos().ForEach(Console.WriteLine);
     }
 
     //Agregar 3 alumnos al diccionario
@@ -21,12 +41,63 @@ internal class Ejemplos
     //Eliminar un alumno por clave y listar por consola los alumnos
     public static void EjemploDictionary()
     {
+        var diccionario = new CasoDictionary();
 
+        diccionario.AgregarAlumno(77, new Alumno(133, "Pedro", 7));
+        diccionario.AgregarAlumno(44, new Alumno(155, "Nacho", 6));
+        diccionario.AgregarAlumno(33, new Alumno(444, "Juan", 9));
+        foreach (var alumno in diccionario.GetAlumnos())
+        {
+            Console.WriteLine(alumno);
+        }
+
+        Console.WriteLine($"{diccionario.BuscarAlumnoPorClave(44)}");
+
+      
+        Console.WriteLine($"{diccionario.BuscarAlumnoPorClave(66)?.Nombre ?? "No existe"}");
+
+        diccionario.EliminarAlumnoPorClave(33);
+        foreach (var alumno in diccionario.GetAlumnos())
+        {
+            Console.WriteLine(alumno);
+        }
     }
 
     //Realizar una llamada a cada método definido en CasoLinq y mostar por consola según corresponda
     public static void EjemploLinq()
     {
+
+        //       *Para cada punto crear un método que permita:
+        //       *1.Obtener el primer libro(GetPrimero)
+        //       * 2.Obtener el último libro(GetUltimo)
+        //       * 3.Obtener la suma de precios(GetTotalPrecios)
+        //*4.Obtener el promedio de precios(GetPromedioPrecios)
+        //*5.Obtener la lista de libros con Id mayor a 15(GetListById)
+        //* 6.Obtener una lista de cada libro con su título y precio en formato moneda(GetLibros)(debe retornar una lista de string)
+        //* 7.Obtener el libro con el precio más alto(GetMayorPrecio)
+        //* 8.Obtener el libro con el precio más bajo(GetMenorPrecio)
+        //* 9.Obtener los libros cuyo precio sea mayor al promedio(GetMayorPromedio)
+        //*10.Obtener los libros ordenados por título de forma descendente
+        //*En todos los casos debe aplicarse LINQ
+        //*/
+        var libros = new List<Libro>();
+        libros.Add(new Libro(12, "Don Quijote De la Mancha", 40.000M));
+        libros.Add(new Libro(13, "La Riqueza de las naciones", 60.000M));
+        libros.Add(new Libro(16, "El Principito", 35.000M));
+
+        var listaLinq = new CasoLinq(libros);
+
+       var primero = listaLinq.GetPrimero();
+       var ultimo = listaLinq.GetUltimo();
+        var precios = listaLinq.GetTotalPrecios();
+        var promedioDePrecios = listaLinq.GetPromedioPrecios();
+        var listaMayor = listaLinq.GetListById();
+        var detalles = listaLinq.GetLibros();
+        var mayorPrecio = listaLinq.GetMayorPrecio();
+        var menorPrecio = listaLinq.GetMenorPrecio();
+        var mayorPromedio = listaLinq.GetMayorPromedio();
+        var listaOrdenada = listaLinq.GetLibrosOrdenadosPorTitulo();
+        
 
     }
 }
